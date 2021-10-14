323dc1b24ed3dd10d3c7aa73ad62567d7f4b962f
kareefardi <kareemfarid@aucegypt.edu>
2021-10-14 13:44:00 +0200

    test case 3

--- test3/run_enumer.sh
+++ test3/run_enumer.sh
@@ -5,6 +5,6 @@ set -xu
 dir=mesh
 output_spice=mesh.spice
 run_template=run_template.sh
-tb=mesh.spice
+tb=tb_mesh.spice
 cfg=mesh.cfg
 ../utils/enumer.py -v -d $dir -o $output_spice -r $run_template $tb $cfg
